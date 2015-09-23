693ee09faf9f2d70d825b4e60196b0d37ef7f520
songyingfei <songyingfei@zyeeda.com>
2015-09-23 15:55:48 +0800

    员工管理show、edit页面的输入框监听

--- src/main/webapp/scripts/app/employee/employee-info.feature/scaffold.js
+++ src/main/webapp/scripts/app/employee/employee-info.feature/scaffold.js
@@ -8,6 +8,9 @@
 
         afterShowDialog: function(dialogType, view, data){
             var me, currentDepartment,
+            agreementDate = data.agreementDate,
+            agreementLast = data.agreementLast,
+            endYear, endMonth,endDay,
             workYear, workMonth, workDay, entryTime,
             msec,
             date = new Date();
@@ -31,11 +34,26 @@
                 // $('input[name= "seniority"]').val(workYear + "年" + workMonth + "月" + workDay + "天");
             }
             if('edit' === dialogType){
+                if(!agreementLast.length == 0 && !agreementDate.length == 0){
+
+                    agreementDate = new Date(agreementDate);
+                    endYear  = agreementDate.getFullYear();
+                    endYear  = endYear + parseInt(agreementLast);
+                    endMonth = agreementDate.getMonth() + 1;
+                    if(endMonth<10){
+                        endMonth = "0" + endMonth;
+                    }
+                    endDay = agreementDate.getDate() - 1;
+                    $('input[name= "agreementEnd"]').val(endYear + "-" + endMonth + "-" + endDay);
+                }
                 entryTime = new Date(data.entryTime);
                 msec    = (date.getTime()-entryTime.getTime());
                 workYear  = Math.floor(msec/(365*24*3600*1000));
                 workMonth = Math.floor(msec%(365*24*3600*1000)/(30*24*3600*1000));
                 workDay   = Math.floor(msec%(30*24*3600*1000)/(24*3600*1000)) + 1;
+
+                $('input[name = "agreementEnd"]', view.$el).attr('disabled',true);
+                $('input[name = "seniority"]', view.$el).attr('disabled',true);
                 me.feature.model.set('seniority', workYear + "年" + workMonth + "月" + workDay + "天");
                 // $('input[name= "seniority"]').val(workYear + "年" + workMonth + "月" + workDay + "天");
             }
