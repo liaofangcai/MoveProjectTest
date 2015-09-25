0a6b5fd51d6d4517117369c08386e10bd2c3dfec
songyingfei <songyingfei@zyeeda.com>
2015-09-25 18:15:16 +0800

    资材管理实体类

---
+++ src/main/java/com/zyeeda/business/asset /entity/AssetManage.java
@@ -0,0 +1,260 @@
+package com.zyeeda.business.asset.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.zyeeda.cdeio.commons.annotation.scaffold.Scaffold;
+import com.zyeeda.cdeio.commons.base.entity.RevisionDomainEntity;
+import com.zyeeda.cdeio.validation.constraint.NullableSize;
+
+@Entity
+@Table(name = "bz_asset_manage")
+@Scaffold("/asset/asset-manage")
+public class AssetManage extends RevisionDomainEntity{
+  /**
+   * 序列化
+   */
+  private static final long serialVersionUID = 3728277543938624745L;
+  /**
+   * 设备编号
+   */
+  private String equipmentNo;
+  /**
+   *设备名称
+   */
+  private String equipmentName;
+  /**
+   * 规格型号
+   */
+  private String equipmentVersion;
+  /**
+   * 机身编号
+   */
+  private String equipmentId;
+  /**
+   *生产厂家
+   */
+  private String manufacturer;
+  /**
+   * 单位
+   */
+  private String unit;
+  /**
+   *购置日期
+   */
+  private Date purchaseDate;
+  /**
+   *采购价格
+   */
+  private Double price;
+  /**
+   * 附件配备
+   */
+  private String attachment;
+  /**
+   * 供应商名称
+   */
+  private String providerName;
+  /**
+   * 供应商地址
+   */
+  private String providerAddress;
+  /**
+   *联系人
+   */
+  private String contacts;
+  /**
+   * 联系方式
+   */
+  private String phoneNum;
+  /**
+   *地点
+   */
+  private String address;
+  /**
+   * 状态(0:闲置，1:使用中，2:待维修，3:转移,4:报废)
+   */
+  private String status;
+  /**
+   * 备注;
+   */
+  private String remark;
+  /**
+   * 对应的assetStatus;
+   */
+  private List<AssetStatus>  assetStatus;
+
+  @NotBlank
+  @Column(name = "f_equipment_no", length = 300)
+  @NullableSize(max = 166)
+	public String getEquipmentNo() {
+		return equipmentNo;
+	}
+	public void setEquipmentNo(String equipmentNo) {
+		this.equipmentNo = equipmentNo;
+	}
+
+  @NotBlank
+  @Column(name = "f_equipment_name", length = 300)
+  @NullableSize(max = 166)
+	public String getEquipmentName() {
+		return equipmentName;
+	}
+	public void setEquipmentName(String equipmentName) {
+		this.equipmentName = equipmentName;
+	}
+
+
+  @NotBlank
+  @Column(name = "f_equipment_version", length = 300)
+  @NullableSize(max = 166)
+	public String getEquipmentVersion() {
+		return equipmentVersion;
+	}
+	public void setEquipmentVersion(String equipmentVersion) {
+		this.equipmentVersion = equipmentVersion;
+	}
+
+
+  @Column(name = "f_equipment_id", length = 300)
+  @NullableSize(max = 166)
+	public String getEquipmentId() {
+		return equipmentId;
+	}
+	public void setEquipmentId(String equipmentId) {
+		this.equipmentId = equipmentId;
+	}
+
+  @Column(name = "f_manufacturer", length = 300)
+  @NullableSize(max = 166)
+	public String getManufacturer() {
+		return manufacturer;
+	}
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+  @NotBlank
+  @Column(name = "f_unit", length = 300)
+  @NullableSize(max = 166)
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+  @NotNull
+  @Column(name = "f_purchasedate")
+  @Temporal(TemporalType.DATE)
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+  @Column(name = "f_price", length = 20)
+  @Min(value = 0)
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+  @Column(name = "f_attachment", length = 300)
+  @NullableSize(max = 166)
+	public String getAttachment() {
+		return attachment;
+	}
+	public void setAttachment(String attachment) {
+		this.attachment = attachment;
+	}
+
+  @Column(name = "f_provider_name", length = 300)
+  @NullableSize(max = 166)
+	public String getProviderName() {
+		return providerName;
+	}
+	public void setProviderName(String providerName) {
+		this.providerName = providerName;
+	}
+
+  @Column(name = "f_provider_address", length = 300)
+  @NullableSize(max = 166)
+	public String getProviderAddress() {
+		return providerAddress;
+	}
+	public void setProviderAddress(String providerAddress) {
+		this.providerAddress = providerAddress;
+	}
+
+  @Column(name = "f_contacts", length = 300)
+  @NullableSize(max = 166)
+	public String getContacts() {
+		return contacts;
+	}
+	public void setContacts(String contacts) {
+		this.contacts = contacts;
+	}
+
+  @Column(name = "f_phone_num", length = 300)
+  @NullableSize(max = 166)
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+  @Column(name = "f_address", length = 300)
+  @NullableSize(max = 166)
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+  @NotBlank
+  @Column(name = "f_status", length = 300)
+  @NullableSize(max = 166)
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+  @Column(name = "f_remark", length = 300)
+  @NullableSize(max = 166)
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+  @OneToMany(mappedBy = "assetManage")
+	public List<AssetStatus> getAssetStatus() {
+		return assetStatus;
+	}
+	public void setAssetStatus(List<AssetStatus> assetStatus) {
+		this.assetStatus = assetStatus;
+	}
+
+}
---
+++ src/main/java/com/zyeeda/business/asset /entity/AssetStatus.java
@@ -0,0 +1,97 @@
+package com.zyeeda.business.asset.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.zyeeda.cdeio.commons.annotation.scaffold.Scaffold;
+import com.zyeeda.cdeio.commons.base.entity.RevisionDomainEntity;
+import com.zyeeda.cdeio.validation.constraint.NullableSize;
+
+
+@Entity
+@Table(name = "bz_asset_status")
+@Scaffold("/asset/asset-status")
+public class AssetStatus extends RevisionDomainEntity{
+  /**
+   * 序列化
+   */
+  private static final long serialVersionUID = 3728277543938624745L;
+  /**
+   * 使用人
+   */
+  private String userName;
+  /**
+   * 开始时间
+   */
+  private Date startDate;
+  /**
+   *结束时间
+   */
+  private Date endDate;
+  /**
+   * 备注
+   */
+  private String remark;
+  /**
+   * 对应的assetManage
+   */
+  private AssetManage assetManage;
+
+  @NotBlank
+  @Column(name = "f_user_name", length = 300)
+  @NullableSize(max = 166)
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+  @NotNull
+  @Temporal(TemporalType.DATE)
+  @Column(name = "f_start_date")
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+  @NotNull
+  @Temporal(TemporalType.DATE)
+  @Column(name = "f_end_date")
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+  @Column(name = "f_remark", length = 300)
+  @NullableSize(max = 166)
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+  @ManyToOne
+  @JoinColumn(name = "f_asset_manage_id")
+	public AssetManage getAssetManage() {
+		return assetManage;
+	}
+	public void setAssetManage(AssetManage assetManage) {
+		this.assetManage = assetManage;
+	}
+
+}
