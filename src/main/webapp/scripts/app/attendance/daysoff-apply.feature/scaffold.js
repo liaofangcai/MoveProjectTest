8b1b4e7975d47a830946e6099cec78b04f4e50ce
songyingfei <songyingfei@zyeeda.com>
2015-10-13 14:19:26 +0800

    调休申请时间合计时间计算方法

--- src/main/webapp/scripts/app/attendance/daysoff-apply.feature/scaffold.js
+++ src/main/webapp/scripts/app/attendance/daysoff-apply.feature/scaffold.js
@@ -67,21 +67,250 @@ define([
         formStatusChanged: function(data, el) {
           var startDate = Date.parse(data.daysoffBeginDate),
               endTime = Date.parse(data.daysoffEndDate),
-              lastdate,lasthour,lastminute,msec;
+              beginHour, beginMinute,
+              lastminute, lasthour, lastdate,
+              startDay, endDay, startDateLasthour, startDateLastminute,
+              endDateLasthour, endDateLastminute, lastTotalHour, lastTotalminute,
+              workDayVal, remainder, divisor, weekendDay, nextDay,
+              totalTime, msec, date = new Date(), date1 = new Date(), date2 = new Date();
 
-          if( startDate != null && endTime != null){
+              if(startDate != null && endTime != null){ //判断开始时间和结束时间都不为空
+                 msec  = endTime.getTime() - startDate.getTime();
+                 endDay = endTime.getDay();
+                 startDay = startDate.getDay();
 
-              if(endTime.getTime()-startDate.getTime()>0){
+                if(msec > 0 && msec < 24*60*60*1000 && (endDay == startDay)){
+                //开始时间小于结束时间并且在同一天
+                  date.setFullYear(endTime.getFullYear());
+                  date.setMonth(endTime.getMonth());
+                  date.setDate(endTime.getDate());
+                  date.setHours(13);
+                  date.setMinutes(30);
 
-                msec       = (endTime.getTime()-startDate.getTime());
-                lastdate   = Math.floor(msec%(30*24*3600*1000)/(24*3600*1000));
-                lasthour   = Math.floor(msec%(24*3600*1000)/(3600*1000));
-                lastminute = Math.floor(msec%(3600*1000)/(60*1000));
-                $('input[name= "totalTime"]').val(lastdate + "天" + lasthour + "小时" + lastminute + "分钟");
-              }else{
+                  date1.setFullYear(startDate.getFullYear());
+                  date1.setMonth(startDate.getMonth());
+                  date1.setDate(startDate.getDate());
+                  date1.setHours(09);
+                  date1.setMinutes(00);
+                  if(endDay  != 0 && endDay !=6 ){//判断是不是周六周日
+
+                      if(startDate.getTime() <= date1.getTime()){ //开始时间在9点之前
+
+                        if(endTime.getHours() < 9){
+                          lastdate = 0;
+                          lasthour = 0;
+                          lastminute = 0;
+                        }else if(endTime.getHours() >= 18){ //结束时间在18点之后
+                          lastdate = 1;
+                          lasthour = 0;
+                          lastminute = 0;
+                        }else if( endTime.getHours() >= 9 && endTime.getHours() < 12 ){//结束时间在9点到12点之间
+                          lastdate = 0;
+                          lasthour  = endTime.getHours() - 9;
+                          lastminute = endTime.getMinutes();
+                        }else if(endTime.getHours() >= 12 && (endTime.getTime() <= date.getTime())){//结束时间在12点到13:30之间
+                          lastdate    = 0;
+                          lasthour    = 3;
+                          lastminute  = 0;
+                        }else{//结束时间在13:30到18:00之间
+                          lastdate   = 0;
+                          lasthour   = Math.floor((endTime.getTime() - date.getTime())%(24*3600*1000)/(3600*1000)) + 3;
+                          lastminute = Math.floor((endTime.getTime() - date.getTime())%(3600*1000)/(60*1000)) + 1;
+                        }
+                       //开始时间在9点到12点之间的
+                    }else if(startDate.getTime() > date1.getTime() && startDate.getHours() < 12){
+
+                        if(endTime.getHours() < 12){
+                          lastdate = 0;
+                          lasthour =  Math.floor(msec%(24*3600*1000)/(3600*1000));
+                          lastminute = Math.floor(msec%(3600*1000)/(60*1000));
+                        }else if(endTime.getHours() >= 12 && (endTime.getTime() <= date.getTime())){
+                          date.setHours(12);
+                          date.setMinutes(00);
+                          lastdate = 0;
+                          lasthour = Math.floor((date.getTime() - startDate.getTime())%(24*3600*1000)/(3600*1000));
+                          lastminute = Math.floor((date.getTime() - startDate.getTime())%(3600*1000)/(60*1000));
+
+                        }else if(endTime.getHours() >= 18){
+
+                          date.setHours(18);
+                          date.setMinutes(00);
+                          lastdate = 0;
+                          lasthour =  Math.floor((date.getTime() - startDate.getTime() - 90*60*1000)%(24*3600*1000)/(3600*1000));
+                          lastminute = Math.floor((date.getTime() - startDate.getTime() - 90*60*1000)%(3600*1000)/(60*1000));
+                        }else{
+                          lastdate = 0;
+                          lasthour = Math.floor((endTime.getTime() - startDate.getTime() - 90*60*1000)%(24*3600*1000)/(3600*1000));
+                          lastminute = Math.floor((endTime.getTime() - startDate.getTime() - 90*60*1000)%(3600*1000)/(60*1000));
+                        }
+
+                      //开始时间在12点到13:30之间的
+                    }else if(startDate.getHours() >= 12 && (startDate.getTime() <= date.getTime())){
+
+                        if(endTime.getHours() >= 12 && (endTime.getTime() <= date.getTime())){
+                          lastdate = 0;
+                          lasthour = 0;
+                          lastminute = 0;
+                        }else if(endTime.getHours()>=18){
+                          lastdate = 0;
+                          lasthour = 4;
+                          lastminute = 30;
+                        }else{
+                          date.setHours(13);
+                          date.setMinutes(29);
+                          lastdate = 0;
+                          lasthour = Math.floor((endTime.getTime() - date.getTime())%(24*3600*1000)/(3600*1000));
+                          lastminute = Math.floor((endTime.getTime() - date.getTime())%(3600*1000)/(60*1000));
+                        }
+
+                    }else if(startDate.getHours() > 18){//开始时间在18：00之后的
+                      lastdate = 0;
+                      lasthour = 0;
+                      lastminute = 0;
+                    }else{  //开始时间在13：30到18：00之间的
+
+                      if(endTime.getHours() >= 18){//结束时间大于18:00的
+                        date.setHours(18);
+                        date.setMinutes(00);
+                        lastdate = 0;
+                        lasthour = Math.floor((date.getTime() - startDate.getTime())%(24*3600*1000)/(3600*1000));
+                        lastminute = Math.floor((date.getTime() - startDate.getTime())%(3600*1000)/(60*1000));
+                      }else{//结束时间小于18:00的
+                        lastdate = 0;
+                        lasthour = Math.floor((endTime.getTime() - startDate.getTime())%(24*3600*1000)/(3600*1000));
+                        lastminute = Math.floor((endTime.getTime() - startDate.getTime())%(3600*1000)/(60*1000));
+                      }
+                  }
+                  $('input[name= "totalTime"]').val(lastdate + "天" + lasthour + "小时" + lastminute + "分钟");
+                }else{
                   $('input[name= "totalTime"]').val(0);
-              }
-          }
+                }
+
+              }else if(msec>0 && (startDate.getDate() != endTime.getDate() || startDay != endDay)){//请假时间不在同一天
+                    date1.setFullYear(startDate.getFullYear());
+                    date1.setMonth(startDate.getMonth());
+                    date1.setDate(startDate.getDate());
+                    date1.setHours(00);
+                    date1.setMinutes(00);
+
+                    date2.setFullYear(endTime.getFullYear());
+                    date2.setMonth(endTime.getMonth());
+                    date2.setDate(endTime.getDate());
+                    date2.setHours(00);
+                    date2.setMinutes(00);
+                    msec = date2.getTime() - date1.getTime();
+
+                    workDayVal =  Math.floor(msec / (24 * 60 * 60 * 1000));
+
+                    remainder  =  workDayVal%7;              //除以7取余表示剩下的天数
+                    divisor    =  Math.floor(workDayVal/7); //取整，表示中间隔了几周
+                    weekendDay =  2 * divisor;             //表示请假期间度过的周末数量
+
+                    for(var i = 0; i < remainder; i ++){//循环处理余下的天数有多少个周六或者周日（最多出现一个周六或者一个周日）
+                      if(((startDay + i)==6)||((startDay + i)==0)||((startDay + i)==7)){
+                          weekendDay = weekendDay + 1;
+                      }
+                    }
+
+                    lastdate = workDayVal - weekendDay;
+                    //计算开始请假当天 所剩余请假时间；
+                    if(startDay == 0 || startDay == 6){//开始时间是周末
+                      startDateLasthour = 0;
+                      startDateLastminute = 0;
+                      //开始请假当天的请假时间为0
+
+                    }else{//开始时间不是周末
+                      date.setFullYear(startDate.getFullYear());
+                      date.setMonth(startDate.getMonth());
+                      date.setDate(startDate.getDate());
+                      date.setHours(13);
+                      date.setMinutes(30);
+                      if(startDate.getHours()<9){//开始时间在9点之前
+
+                        startDateLasthour = 7;
+                        startDateLastminute = 30;
+                      }else if(startDate.getHours() >= 9 && startDate.getHours() < 12){
+                        date.setHours(12);
+                        date.setMinutes(00);
+                        startDateLasthour = Math.floor((date.getTime() - startDate.getTime())%(24*3600*1000)/(3600*1000)) + 4;
+                        startDateLastminute = Math.floor((date.getTime() - startDate.getTime())%(3600*1000)/(60*1000)) + 30;
+                      }else if(startDate.getHours() >= 12 && date.getTime() >= startDate.getTime()){
+                          startDateLasthour = 4;
+                          startDateLastminute = 30;
+                      }else if(startDate.getHours() >= 18){
+                          startDateLasthour = 0;
+                          startDateLastminute = 0;
+                      }else{
+                          date.setHours(18);
+                          date.setMinutes(00);
+                          startDateLasthour =  Math.floor((date.getTime() - startDate.getTime())%(24*3600*1000)/(3600*1000));
+                          startDateLastminute = Math.floor((date.getTime() - startDate.getTime())%(3600*1000)/(60*1000));
+                      }
+                    }
+                      //计算请假结束当天的请假时间
+                    if(endDay == 0 || endDay == 6){//结束时间是周末
+
+                      endTimeLasthour = 0;
+                      endTimeLastminute = 0;
+
+                      //结束请假当天的请假时间为0
+                    }else{//结束时间不是周末
+
+                      date.setFullYear(endTime.getFullYear());
+                      date.setMonth(endTime.getMonth());
+                      date.setDate(endTime.getDate());
+                      date.setHours(13);
+                      date.setMinutes(30);
+                      if(endTime.getHours()<9){//结束时间在9点之前
+                        endTimeLasthour = 0;
+                        endTimeLastminute = 0;
+                      }else if(endTime.getHours() >= 9 && endTime.getHours() < 12){
+                        endTimeLasthour = endTime.getHours() - 9;
+                        endTimeLastminute = endTime.getMinutes();
+
+                      }else if(endTime.getHours() >= 12 && date.getTime() >= endTime.getTime()){
+                          endTimeLasthour = 3;
+                          endTimeLastminute = 0;
+
+                      }else if(endTime.getHours() >= 18){
+                          endTimeLasthour = 7;
+                          endTimeLastminute = 30;
+                      }else if(endTime.getHours()<18 && date.getTime() < endTime.getTime()){
+                          endTimeLasthour =  Math.floor((endTime.getTime()-date.getTime())%(24*3600*1000)/(3600*1000)) + 3;
+                          endTimeLastminute =Math.floor((endTime.getTime()-date.getTime())%(3600*1000)/(60*1000))+ 1;
+                      }
+                  }
+                      lastTotalminute = endTimeLastminute +  startDateLastminute;
+
+                      lastTotalHour = endTimeLasthour + startDateLasthour;
+
+                      if((lastTotalminute + lastTotalHour * 60) ==  7.5 * 60 * 2){
+                        lastdate = lastdate + 1;
+                        lasthour = 0;
+                        lastminute = 0;
+                      }else if((lastTotalminute + lastTotalHour * 60) < 7.5 * 60 && (lastTotalminute + lastTotalHour * 60) > 0){
+                        lastdate = lastdate - 1;
+                        lasthour = Math.floor((lastTotalHour * 60 + lastTotalminute) / 60);
+                        lastminute = (lastTotalHour * 60 + lastTotalminute) % 60;
+                      }else if((lastTotalminute + lastTotalHour * 60) == 7.5 * 60 ){
+                        lastminute = 0;
+                        lasthour = 0;
+                      }else if((lastTotalminute + lastTotalHour * 60) == 0){
+                        lastdate = lastdate - 1;
+                        lasthour = 0;
+                        lastminute = 0;
+                      }else{
+                        lasthour = Math.floor((lastTotalHour * 60 + lastTotalminute - 7.5 * 60) / 60);
+                        lastminute = (lastTotalHour * 60 + lastTotalminute - 7.5 * 60) % 60;
+                      }
+
+                    $('input[name= "totalTime"]').val(lastdate + "天" + lasthour + "小时" + lastminute + "分钟");
+                }else{
+                  $('input[name= "totalTime"]').val(0);
+                }
+            }else{
+               $('input[name= "totalTime"]').val(0);
+            }
         },
         retrieve: function(){
             var me = this;
