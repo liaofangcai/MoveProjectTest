b12757a2fd0d22eb50c41a6334f0eb3c6500b60f
liaofangcai <liaofangcai@zyeeda.com>
2015-12-21 15:09:49 +0800

    自动批次作业表

---
+++ src/main/java/com/zyeeda/business/Informationwork/entity/AutomaticBatchOperation.java
@@ -0,0 +1,104 @@
+package com.zyeeda.business.informationwork.entity;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.persistence.Transient;
+import javax.persistence.Temporal;
+import javax.persistence.OrderBy;
+import javax.persistence.TemporalType;
+import org.hibernate.validator.constraints.NotBlank;
+
+
+import com.zyeeda.cdeio.commons.annotation.scaffold.Scaffold;
+import com.zyeeda.cdeio.commons.base.entity.RevisionDomainEntity;
+import com.zyeeda.cdeio.validation.constraint.NullableSize;
+@Entity
+@Table(name = "bz_automaticBatchOperation")
+@Scaffold("/informationwork/automaticBatchOperation")
+public class AutomaticBatchOperation extends RevisionDomainEntity{
+
+  private static final long serialVersionUID = 728277543938624745L;
+  //编号
+  private String systemUpdateRecordsNo;
+  //主机名称
+  private String systemUpdateRecordsName;
+  //主机地址
+  private String address;
+  //job名称
+  private String operatingPersonnel;
+  //JOB内容
+  private String updateContent;
+  //运行时间
+  private Date operateTime;
+  //备注
+  private String mark;
+
+    @NotBlank
+    @Column(name = "F_SYSTEMUPDATERECORDSNO", length = 300)
+	public String getSystemUpdateRecordsNo() {
+		return systemUpdateRecordsNo;
+	}
+	public void setSystemUpdateRecordsNo(String systemUpdateRecordsNo) {
+		this.systemUpdateRecordsNo = systemUpdateRecordsNo;
+	}
+	@NotBlank
+    @Column(name = "F_SYSTEMUPDATERECORDSNAME", length = 300)
+	public String getSystemUpdateRecordsName() {
+		return systemUpdateRecordsName;
+	}
+	public void setSystemUpdateRecordsName(String systemUpdateRecordsName) {
+		this.systemUpdateRecordsName = systemUpdateRecordsName;
+	}
+	@NotBlank
+    @Column(name = "F_ADDRESS", length = 300)
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@NotBlank
+    @Column(name = "F_OPERATINGPERSONNEL", length = 300)
+	public String getOperatingPersonnel() {
+		return operatingPersonnel;
+	}
+	public void setOperatingPersonnel(String operatingPersonnel) {
+		this.operatingPersonnel = operatingPersonnel;
+	}
+	@NotNull
+    @Column(name = "F_OPERATETIME")
+    @Temporal(TemporalType.DATE)
+	public Date getOperateTime() {
+		return operateTime;
+	}
+	public void setOperateTime(Date operateTime) {
+		this.operateTime = operateTime;
+	}
+	@NotBlank
+    @Column(name = "F_UPDATECONTENT")
+    @NullableSize(max = 1333)
+	public String getUpdateContent() {
+		return updateContent;
+	}
+	public void setUpdateContent(String updateContent) {
+		this.updateContent = updateContent;
+	}
+    @Column(name = "F_MARK")
+	public String getMark() {
+		return mark;
+	}
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+}
---
+++ src/main/webapp/WEB-INF/app/Informationwork/automaticBatchOperation.feature/scaffold.js
@@ -0,0 +1,111 @@
+var {mark}                    = require('cdeio/mark');
+var {json}                    = require('cdeio/response');
+var _                         = require('underscore');
+var fs                        = require('fs');
+var objects                   = require('cdeio/util/objects');
+var response                  = require('ringo/jsgi/response');
+var {getOptionInProperties}   = require('cdeio/config');
+var {join}                    = require('cdeio/util/paths');
+var {createService}           = require('informationwork/engineroom-log.feature/service');
+
+var {AutomaticBatchOperation}             = com.zyeeda.business.informationwork.entity;
+var {SecurityUtils}           = org.apache.shiro;
+
+var {SimpleDateFormat}        = java.text;
+var {Date}                    = java.util;
+var {ArrayList}               = java.util;
+var URLDecoder                = java.net.URLDecoder;
+
+exports.style = 'grid';
+//启用前台交互扩展，这样就可以给按钮添加事件了
+exports.enableFrontendExtension = true;
+
+exports.haveFilter = true;
+exports.filters = {
+  defaults: {
+    '!automaticBatchOperationFilter': ['']
+  }
+};
+
+exports.service = function(service){
+    return _.extend(service, createService());
+};
+
+exports.entityLabel = '自动批次作业表';
+
+exports.labels = {
+  systemUpdateRecordsNo: '编号',
+  systemUpdateRecordsName: '所在主机名称',
+  address: '所在主机地址',
+  operatingPersonnel:'job名称',
+  updateContent:'job内容',
+  operateTime:'运行时间',
+  mark: '备注'
+
+};
+
+exports.forms = {
+  edit: {
+      groups: [
+        {name: 'defaults',columns:2}
+      ],
+      size: 'medium'
+  },
+  show: {
+      groups: [
+        {name: 'defaults',columns:2}
+      ],
+      size: 'medium'
+  },
+  add: {
+      groups: [
+        {name: 'defaults',columns:2}
+      ],
+      size: 'medium'
+  },
+  filter: {
+    groups: [{name: 'filter'}],
+    size: 'medium'
+  }
+};
+
+exports.fieldGroups = {
+  defaults:[
+  'systemUpdateRecordsNo', 'systemUpdateRecordsName','address','operatingPersonnel','operateTime','mark',
+  {name:'updateContent', type: 'textarea',colspan:2}
+
+  ],
+  //配置查询条件
+  filter: [
+    'systemUpdateRecordsNo','operatingPersonnel', {name: 'operateTime', type: 'date-range'}
+   ]
+};
+exports.grid = {
+    columns: ['systemUpdateRecordsNo', 'systemUpdateRecordsName', 'address','mark',{name: 'updateContent',
+    type: 'textarea'}],
+    filterToolbar: true,
+    fixedHeader: true,
+    numberColumn: true,
+    multiple: true,
+    defaultOrder: 'createdTime-desc'
+};
+exports.exporting = {
+  template: 'informationwork/automaticBatchOperation/automaticBatchOperationModule.xls',
+  fileName: '自动批次作业表'
+};
+exports.operators = {
+    exportExcel: { label: '导出', icon: 'zicon-outexcel', group: '30-refresh', order: 10, show: 'unselected', style: 'btn-pink' }
+};
+exports.doWithRouter = function(router) {
+  //导出
+  router.get('/export-excel', mark('services', 'commons/export-excel', 'informationwork/automaticBatchOperation').on(function (exportXlsSvc, automaticBatchOperationSvc, request) {
+      var options = request.params,
+          result;
+
+      options = exportXlsSvc.dealParameters(options, automaticBatchOperationSvc, new AutomaticBatchOperation());
+
+      result = automaticBatchOperationSvc.exportExcel(options, exports.exporting.template, exports.exporting.fileName);
+
+      return json({flag: result.flag, filename: result.filename});
+  }))
+}
