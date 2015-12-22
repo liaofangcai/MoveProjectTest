096a7d51469a185a018ddcf1f89bf46b37909177
liaofangcai <liaofangcai@zyeeda.com>
2015-12-22 18:18:16 +0800

    系统备份计划

--- src/main/java/com/zyeeda/business/Informationwork/entity/PlanBackup.java
+++ src/main/java/com/zyeeda/business/Informationwork/entity/PlanBackup.java
@@ -23,14 +23,17 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.zyeeda.cdeio.commons.annotation.scaffold.Scaffold;
 import com.zyeeda.cdeio.commons.base.entity.RevisionDomainEntity;
 import com.zyeeda.cdeio.validation.constraint.NullableSize;
+
 @Entity
-@Table(name = "bz_planBackup")
+@Table(name = "BZ_PLANBACKUP")
 @Scaffold("/informationwork/planBackup")
 public class PlanBackup extends RevisionDomainEntity{
 
   private static final long serialVersionUID = 2827543938624745L;
   //编号
   private String systemUpdateRecordsNo;
+  //系统名称
+  private String name;
   //计划备份时间
   private Date systemUpdateRecordsName;
   //备份方式
@@ -44,61 +47,75 @@ public class PlanBackup extends RevisionDomainEntity{
   //备注
   private String mark;
 
-    @NotBlank
-    @Column(name = "f_systemUpdateRecordsNo", length = 300)
-	public String getSystemUpdateRecordsNo() {
-		return systemUpdateRecordsNo;
-	}
-	public void setSystemUpdateRecordsNo(String systemUpdateRecordsNo) {
-		this.systemUpdateRecordsNo = systemUpdateRecordsNo;
-	}
-	@NotNull
-	@DateTime
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm",timezone = "GMT+8")
-    @Column(name = "f_systemUpdateRecordsName")
-	public Date getSystemUpdateRecordsName() {
-		return systemUpdateRecordsName;
-	}
-	public void setSystemUpdateRecordsName(Date systemUpdateRecordsName) {
-		this.systemUpdateRecordsName = systemUpdateRecordsName;
-	}
-	@NotBlank
-    @Column(name = "f_address")
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	@NotBlank
-    @Column(name = "f_operatingPersonnel", length = 300)
-	public String getOperatingPersonnel() {
-		return operatingPersonnel;
-	}
-	public void setOperatingPersonnel(String operatingPersonnel) {
-		this.operatingPersonnel = operatingPersonnel;
-	}
+  @NotBlank
+  @Column(name = "F_NAME")
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    @Column(name = "f_operateTime")
-	public String getOperateTime() {
-		return operateTime;
-	}
-	public void setOperateTime(String operateTime) {
-		this.operateTime = operateTime;
-	}
-	@NotBlank
-    @Column(name = "f_updateContent")
-	public String getUpdateContent() {
-		return updateContent;
-	}
-	public void setUpdateContent(String updateContent) {
-		this.updateContent = updateContent;
-	}
-    @Column(name = "f_mark")
-	public String getMark() {
-		return mark;
-	}
-	public void setMark(String mark) {
-		this.mark = mark;
-	}
+  @NotBlank
+  @Column(name = "F_SYSTEMUPDATERECORDSNO", length = 300)
+  public String getSystemUpdateRecordsNo() {
+    return systemUpdateRecordsNo;
+  }
+  public void setSystemUpdateRecordsNo(String systemUpdateRecordsNo) {
+    this.systemUpdateRecordsNo = systemUpdateRecordsNo;
+  }
+
+  @NotNull
+  @DateTime
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm",timezone = "GMT+8")
+  @Column(name = "F_SYSTEMUPDATERECORDSNAME")
+  public Date getSystemUpdateRecordsName() {
+    return systemUpdateRecordsName;
+  }
+  public void setSystemUpdateRecordsName(Date systemUpdateRecordsName) {
+    this.systemUpdateRecordsName = systemUpdateRecordsName;
+  }
+
+  @NotBlank
+  @Column(name = "F_ADDRESS")
+  public String getAddress() {
+    return address;
+  }
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  @NotBlank
+  @Column(name = "F_OPERATINGPERSONNEL", length = 300)
+  public String getOperatingPersonnel() {
+    return operatingPersonnel;
+  }
+  public void setOperatingPersonnel(String operatingPersonnel) {
+    this.operatingPersonnel = operatingPersonnel;
+  }
+
+  @Column(name = "F_OPERATETIME")
+  public String getOperateTime() {
+    return operateTime;
+  }
+  public void setOperateTime(String operateTime) {
+    this.operateTime = operateTime;
+  }
+
+  @NotBlank
+  @Column(name = "F_UPDATECONTENT")
+  public String getUpdateContent() {
+    return updateContent;
+  }
+  public void setUpdateContent(String updateContent) {
+    this.updateContent = updateContent;
+  }
+
+  @Column(name = "F_MARK")
+  public String getMark() {
+    return mark;
+  }
+  public void setMark(String mark) {
+    this.mark = mark;
+  }
 }
