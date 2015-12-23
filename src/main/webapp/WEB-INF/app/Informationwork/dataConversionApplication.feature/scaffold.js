2f79c545ec6b1848a0cc59a66e6995e0736e981f
liaofangcai <liaofangcai@zyeeda.com>
2015-12-23 09:46:14 +0800

    系统数据转换申请表单

--- src/main/webapp/WEB-INF/app/Informationwork/dataConversionApplication.feature/scaffold.js
+++ src/main/webapp/WEB-INF/app/Informationwork/dataConversionApplication.feature/scaffold.js
@@ -1,26 +1,27 @@
-var {mark}                    = require('cdeio/mark');
-var {json}                    = require('cdeio/response');
-var _                         = require('underscore');
-var fs                        = require('fs');
-var objects                   = require('cdeio/util/objects');
-var response                  = require('ringo/jsgi/response');
-var {getOptionInProperties}   = require('cdeio/config');
-var {join}                    = require('cdeio/util/paths');
-var {createService}           = require('informationwork/engineroom-log.feature/service');
-
-var {DataConversionApplication}             = com.zyeeda.business.informationwork.entity;
-var {SecurityUtils}           = org.apache.shiro;
-
-var {SimpleDateFormat}        = java.text;
-var {Date}                    = java.util;
-var {ArrayList}               = java.util;
-var URLDecoder                = java.net.URLDecoder;
+var {mark}                                       = require('cdeio/mark');
+var {json}                                         = require('cdeio/response');
+var _                                                = require('underscore');
+var fs                                               = require('fs');
+var objects                                      = require('cdeio/util/objects');
+var response                                   = require('ringo/jsgi/response');
+var {getOptionInProperties}            = require('cdeio/config');
+var {join}                                          = require('cdeio/util/paths');
+var {createService}                         = require('informationwork/engineroom-log.feature/service');
+var {DataConversionApplication}    = com.zyeeda.business.informationwork.entity;
+var {SecurityUtils}                            = org.apache.shiro;
+var {SimpleDateFormat}                   = java.text;
+var {Date}                                          = java.util;
+var {ArrayList}                                   = java.util;
+var URLDecoder                               = java.net.URLDecoder;
 
 exports.style = 'grid';
-//启用前台交互扩展，这样就可以给按钮添加事件了
+
 exports.enableFrontendExtension = true;
 
 exports.haveFilter = true;
+
+exports.entityLabel = '系统数据转换申请';
+
 exports.filters = {
   defaults: {
     '!dataConversionApplicationFilter': ['']
@@ -28,13 +29,10 @@ exports.filters = {
 };
 
 exports.service = function(service){
-    return _.extend(service, createService());
+  return _.extend(service, createService());
 };
 
-exports.entityLabel = '系统数据转换申请';
-
 exports.labels = {
-
   systemRenewalApplicationNo: '编号',
   applicationTime: '申请日期',
   //系统数据转换计划
@@ -51,35 +49,36 @@ exports.labels = {
   departmentHead:'部门主管意见',
   informationDepartment:'信息部门意见',
   companyLeadershipApproval: '公司领导审批',
-
-
 };
 
 exports.forms = {
   edit: {
-      groups: [
-        {name: 'defaults',columns:2},
-        {name: 'remoteAccessApplications',label:'数据转换计划',columns:2},
-        {name: 'auditOpinion',label: '审核意见'}
-      ],
-      size: 'medium'
+    groups: [
+    {name: 'defaults',columns:2},
+    {name: 'remoteAccessApplications',label:'数据转换计划',columns:2},
+    {name: 'auditOpinion',label: '审核意见'}
+    ],
+    size: 'medium'
   },
+
   show: {
-      groups: [
-        {name: 'defaults',columns:2},
-        {name: 'remoteAccessApplications',label:'数据转换计划',columns:2},
-        {name: 'auditOpinion',label: '审核意见'}
-      ],
-      size: 'medium'
+    groups: [
+    {name: 'defaults',columns:2},
+    {name: 'remoteAccessApplications',label:'数据转换计划',columns:2},
+    {name: 'auditOpinion',label: '审核意见'}
+    ],
+    size: 'medium'
   },
+
   add: {
-      groups: [
-        {name: 'defaults',columns:2},
-        {name: 'remoteAccessApplications',label:'数据转换计划',columns:2},
-        {name: 'auditOpinion',label: '审核意见'}
-      ],
-      size: 'medium'
+    groups: [
+    {name: 'defaults',columns:2},
+    {name: 'remoteAccessApplications',label:'数据转换计划',columns:2},
+    {name: 'auditOpinion',label: '审核意见'}
+    ],
+    size: 'medium'
   },
+
   filter: {
     groups: [{name: 'filter',columns:2}],
     size: 'medium'
@@ -87,47 +86,55 @@ exports.forms = {
 };
 
 exports.fieldGroups = {
-  defaults:['systemRenewalApplicationNo','applicationTime'],
+  defaults: ['systemRenewalApplicationNo','applicationTime'],
   //申请部门
-  remoteAccessApplications:['informationSystemName','masterName',
+  remoteAccessApplications: [
+  'informationSystemName','masterName',
   {name: 'address',type :'textarea' ,colspan : 2},{name: 'incidence',type :'textarea' ,colspan : 2},
   {name: 'updateContent',type :'textarea' ,colspan : 2}, {name: 'proposer', type: 'datetimepicker', statusChanger: true},
-  {name: 'applicationDepartment', type: 'datetimepicker', statusChanger: true},'identifierPersonnal','identifierTime'],
+  {name: 'applicationDepartment', type: 'datetimepicker', statusChanger: true},'identifierPersonnal','identifierTime'
+  ],
   //审核意见
-  auditOpinion:[{name: 'departmentHead', type: 'textarea'},
+  auditOpinion: [
+  {name: 'departmentHead', type: 'textarea'},
   {name: 'informationDepartment',type:  'textarea'},
-  {name : 'companyLeadershipApproval',type:  'textarea'}],
-  //配置查询条件
+  {name : 'companyLeadershipApproval',type:  'textarea'}
+  ],
+
   filter: [
-    'systemRenewalApplicationNo','informationSystemName', {name: 'applicationTime', type: 'date-range'},
-    {name: 'applicationDepartment',type: 'date-range'},'identifierPersonnal','identifierTime' ]
+  'systemRenewalApplicationNo','informationSystemName', 'identifierPersonnal','identifierTime',
+  {name: 'applicationTime', type: 'date-range'}, {name: 'applicationDepartment',type: 'date-range'}
+  ]
 };
+
 exports.grid = {
-    columns: ['systemRenewalApplicationNo', 'applicationTime','informationSystemName','proposer','applicationDepartment',
-    'identifierTime','identifierPersonnal'],
-    filterToolbar: true,
-    fixedHeader: true,
-    numberColumn: true,
-    multiple: true,
-    defaultOrder: 'createdTime-desc'
+  columns: [
+  'systemRenewalApplicationNo', 'applicationTime','informationSystemName','proposer','applicationDepartment',
+  'identifierTime','identifierPersonnal'
+  ],
+  filterToolbar: true,
+  fixedHeader: true,
+  numberColumn: true,
+  multiple: true,
+  defaultOrder: 'createdTime-desc'
 };
+
 exports.exporting = {
   template: 'informationwork/dataConversionApplication/dataConversionApplicationModule.xls',
   fileName: '系统数据转换申请'
 };
+
 exports.operators = {
-    exportExcel: { label: '导出', icon: 'zicon-outexcel', group: '30-refresh', order: 10, show: 'unselected', style: 'btn-pink' }
+  exportExcel: { label: '导出', icon: 'zicon-outexcel', group: '30-refresh', order: 10, show: 'unselected', style: 'btn-pink' }
 };
+
 exports.doWithRouter = function(router) {
-  //导出
   router.get('/export-excel', mark('services', 'commons/export-excel', 'informationwork/dataConversionApplication').on(function (exportXlsSvc, dataConversionApplicationSvc, request) {
-      var options = request.params,
-          result;
+    var options = request.params, result;
 
-      options = exportXlsSvc.dealParameters(options, dataConversionApplicationSvc, new DataConversionApplication());
+    options = exportXlsSvc.dealParameters(options, dataConversionApplicationSvc, new DataConversionApplication());
+    result = dataConversionApplicationSvc.exportExcel(options, exports.exporting.template, exports.exporting.fileName);
 
-      result = dataConversionApplicationSvc.exportExcel(options, exports.exporting.template, exports.exporting.fileName);
-
-      return json({flag: result.flag, filename: result.filename});
+    return json({flag: result.flag, filename: result.filename});
   }))
 }
