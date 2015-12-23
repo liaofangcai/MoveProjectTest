0f13b8652cc8e86bab786bd59c57dbe885486558
liaofangcai <liaofangcai@zyeeda.com>
2015-12-23 15:02:30 +0800

    系统主机更新申请表单

--- src/main/java/com/zyeeda/business/Informationwork/entity/SystemRenewalApplication.java
+++ src/main/java/com/zyeeda/business/Informationwork/entity/SystemRenewalApplication.java
@@ -23,8 +23,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.zyeeda.cdeio.commons.annotation.scaffold.Scaffold;
 import com.zyeeda.cdeio.commons.base.entity.RevisionDomainEntity;
 import com.zyeeda.cdeio.validation.constraint.NullableSize;
+
 @Entity
-@Table(name = "bz_systemRenewalApplication")
+@Table(name = "BZ_RENEWALAPPLICATION")
 @Scaffold("/informationwork/systemRenewalApplication")
 public class SystemRenewalApplication extends RevisionDomainEntity{
 
@@ -33,7 +34,6 @@ public class SystemRenewalApplication extends RevisionDomainEntity{
 	private String systemRenewalApplicationNo;
 	//申请日期
 	private Date systemRenewalApplicationDate;
-
 	//远程访问申请
 	//信息系统名称
 	private String informationSystemName;
@@ -45,115 +45,124 @@ public class SystemRenewalApplication extends RevisionDomainEntity{
 	private String updateContent;
 	//影响范围
 	private String  incidence;
-    //申请人
-    private String proposer;
-    //申请部门
-    private String applicationDepartment;
+	//申请人
+	private String proposer;
+	//申请部门
+	private String applicationDepartment;
+	// 审核意见
+	//部门主管意见
+	private String departmentHead;
+	//信息部门意见
+	private String informationDepartment;
+	//公司领导审批
+	private String companyLeadershipApproval;
 
-    // 审核意见
-    //部门主管意见
-    private String departmentHead;
-    //信息部门意见
-    private String informationDepartment;
-    //公司领导审批
-    private String companyLeadershipApproval;
-
-    @NotBlank
-    @Column(name="f_systemRenewalApplicationNo",length=300)
+	@NotBlank
+	@Column(name="F_RENEWALAPPLICATIONNO",length=300)
 	public String getSystemRenewalApplicationNo() {
 		return systemRenewalApplicationNo;
 	}
 	public void setSystemRenewalApplicationNo(String systemRenewalApplicationNo) {
 		this.systemRenewalApplicationNo = systemRenewalApplicationNo;
 	}
+
 	@NotNull
 	@Temporal(TemporalType.DATE)
-	@Column(name="f_systemRenewalApplicationDate")
+	@Column(name="F_RENEWALAPPLICATIONDATE")
 	public Date getSystemRenewalApplicationDate() {
 		return systemRenewalApplicationDate;
 	}
 	public void setSystemRenewalApplicationDate(Date systemRenewalApplicationDate) {
 		this.systemRenewalApplicationDate = systemRenewalApplicationDate;
 	}
+
 	@NotBlank
-	@Column(name="f_informationSystemName",length=300)
+	@Column(name="F_SYSTEMNAME",length=300)
 	public String getInformationSystemName() {
 		return informationSystemName;
 	}
 	public void setInformationSystemName(String informationSystemName) {
 		this.informationSystemName = informationSystemName;
 	}
+
 	@NotBlank
-	@Column(name ="f_masterName")
+	@Column(name ="F_MASTERNAME")
 	public String getMasterName() {
 		return masterName;
 	}
 	public void setMasterName(String masterName) {
 		this.masterName = masterName;
 	}
+
 	@NotBlank
-	@Column(name="f_address")
+	@Column(name="F_ADDRESS")
 	public String getAddress() {
 		return address;
 	}
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	@NotBlank
-	@Column(name="f_updateContent")
+	@Column(name="F_UPDATECONTENT")
 	public String getUpdateContent() {
 		return updateContent;
 	}
 	public void setUpdateContent(String updateContent) {
 		this.updateContent = updateContent;
 	}
+
 	@NotBlank
-	@Column(name="f_incidence")
+	@Column(name="F_INCIDENCE")
 	public String getIncidence() {
 		return incidence;
 	}
 	public void setIncidence(String incidence) {
 		this.incidence = incidence;
 	}
+
 	@NotBlank
-	@Column(name="f_proposer",length=300)
+	@Column(name="F_PROPOSER",length=300)
 	public String getProposer() {
 		return proposer;
 	}
 	public void setProposer(String proposer) {
 		this.proposer = proposer;
 	}
+
 	@NotBlank
-	@Column(name="f_applicationDepartment")
+	@Column(name="F_APPLICATION")
 	public String getApplicationDepartment() {
 		return applicationDepartment;
 	}
 	public void setApplicationDepartment(String applicationDepartment) {
 		this.applicationDepartment = applicationDepartment;
 	}
+
 	@NotBlank
-	@Column(name="f_departmentHead")
+	@Column(name="F_DEPARTMENTHEAD")
 	public String getDepartmentHead() {
 		return departmentHead;
 	}
 	public void setDepartmentHead(String departmentHead) {
 		this.departmentHead = departmentHead;
 	}
+
 	@NotBlank
-	@Column(name="f_informationDepartment")
+	@Column(name="F_INFORMATION")
 	public String getInformationDepartment() {
 		return informationDepartment;
 	}
 	public void setInformationDepartment(String informationDepartment) {
 		this.informationDepartment = informationDepartment;
 	}
+
 	@NotBlank
-	@Column(name="f_companyLeadershipApproval")
+	@Column(name="F_LEADERSHIPAPPROVAL")
 	public String getCompanyLeadershipApproval() {
 		return companyLeadershipApproval;
 	}
 	public void setCompanyLeadershipApproval(String companyLeadershipApproval) {
 		this.companyLeadershipApproval = companyLeadershipApproval;
 	}
-
 }
--- src/main/webapp/WEB-INF/app/Informationwork/systemRenewalApplication.feature/scaffold.js
+++ src/main/webapp/WEB-INF/app/Informationwork/systemRenewalApplication.feature/scaffold.js
@@ -1,26 +1,27 @@
-var {mark}                    = require('cdeio/mark');
-var {json}                    = require('cdeio/response');
-var _                         = require('underscore');
-var fs                        = require('fs');
-var objects                   = require('cdeio/util/objects');
-var response                  = require('ringo/jsgi/response');
-var {getOptionInProperties}   = require('cdeio/config');
-var {join}                    = require('cdeio/util/paths');
-var {createService}           = require('informationwork/engineroom-log.feature/service');
-
-var {SystemRenewalApplication}             = com.zyeeda.business.informationwork.entity;
-var {SecurityUtils}           = org.apache.shiro;
-
-var {SimpleDateFormat}        = java.text;
-var {Date}                    = java.util;
-var {ArrayList}               = java.util;
-var URLDecoder                = java.net.URLDecoder;
+var {mark}                                 = require('cdeio/mark');
+var {json}                                   = require('cdeio/response');
+var _                                           = require('underscore');
+var fs                                          = require('fs');
+var objects                                 = require('cdeio/util/objects');
+var response                              = require('ringo/jsgi/response');
+var {getOptionInProperties}        = require('cdeio/config');
+var {join}                                      = require('cdeio/util/paths');
+var {createService}                     = require('informationwork/engineroom-log.feature/service');
+var {SystemRenewalApplication} = com.zyeeda.business.informationwork.entity;
+var {SecurityUtils}                         = org.apache.shiro;
+var {SimpleDateFormat}                = java.text;
+var {Date}                                       = java.util;
+var {ArrayList}                                = java.util;
+var URLDecoder                            = java.net.URLDecoder;
 
 exports.style = 'grid';
-//启用前台交互扩展，这样就可以给按钮添加事件了
+
 exports.enableFrontendExtension = true;
 
 exports.haveFilter = true;
+
+exports.entityLabel = '系统主机更新申请';
+
 exports.filters = {
   defaults: {
     '!systemRenewalApplicationFilter': ['']
@@ -28,13 +29,10 @@ exports.filters = {
 };
 
 exports.service = function(service){
-    return _.extend(service, createService());
+  return _.extend(service, createService());
 };
 
-exports.entityLabel = '系统主机更新申请';
-
 exports.labels = {
-
   systemRenewalApplicationNo: '编号',
   systemRenewalApplicationDate: '申请日期',
   //远程访问申请
@@ -48,35 +46,35 @@ exports.labels = {
   //审核意见
   departmentHead:'部门主管意见',
   informationDepartment:'信息部门意见',
-  companyLeadershipApproval: '公司领导审批',
-
-
+  companyLeadershipApproval: '公司领导审批'
 };
 
 exports.forms = {
   edit: {
-      groups: [
-        {name: 'defaults',columns: 2},
-        {name: 'remoteAccessApplications',label: '申请部门',columns:2},
-        {name: 'auditOpinion',label: '审核意见'}
-      ],
-      size: 'medium'
+    groups: [
+    {name: 'defaults',columns: 2},
+    {name: 'remoteAccessApplications',label: '申请部门',columns:2},
+    {name: 'auditOpinion',label: '审核意见'}
+    ],
+    size: 'medium'
   },
+
   show: {
-      groups: [
-        {name: 'defaults',columns:2},
-        {name: 'remoteAccessApplications',label: '申请部门',columns:2},
-        {name: 'auditOpinion',label: '审核意见'}
-      ],
-      size: 'medium'
+    groups: [
+    {name: 'defaults',columns:2},
+    {name: 'remoteAccessApplications',label: '申请部门',columns:2},
+    {name: 'auditOpinion',label: '审核意见'}
+    ],
+    size: 'medium'
   },
+
   add: {
-      groups: [
-        {name: 'defaults',columns:2},
-        {name: 'remoteAccessApplications',label: '申请部门',columns:2},
-        {name: 'auditOpinion',label: '审核意见'}
-      ],
-      size: 'medium'
+    groups: [
+    {name: 'defaults',columns:2},
+    {name: 'remoteAccessApplications',label: '申请部门',columns:2},
+    {name: 'auditOpinion',label: '审核意见'}
+    ],
+    size: 'medium'
   },
   filter: {
     groups: [{name: 'filter'}],
@@ -85,44 +83,53 @@ exports.forms = {
 };
 
 exports.fieldGroups = {
-  defaults:['systemRenewalApplicationNo', 'systemRenewalApplicationDate'],
+  defaults: [
+  'systemRenewalApplicationNo', 'systemRenewalApplicationDate'
+  ],
   //申请部门
-  remoteAccessApplications:['informationSystemName', 'masterName',
-  'address','incidence', {name: 'updateContent',type :'textarea' ,colspan : 2}, 'proposer','applicationDepartment'],
+  remoteAccessApplications: [
+  'informationSystemName', 'masterName',
+  {name: 'address', type: 'textarea', colspan: 2},
+  {name: 'incidence', type :'textarea' , colspan : 2}, {name: 'updateContent', type :'textarea' , colspan : 2},
+  'proposer','applicationDepartment'
+  ],
   //审核意见
-  auditOpinion:[{name: 'departmentHead', type: 'textarea'},
+  auditOpinion: [
+  {name: 'departmentHead', type: 'textarea'},
   {name: 'informationDepartment',type:  'textarea'},
-  {name : 'companyLeadershipApproval',type:  'textarea'}],
+  {name : 'companyLeadershipApproval', type:  'textarea'}
+  ],
   //配置查询条件
   filter: [
     'systemRenewalApplicationNo', 'proposer', {name: 'systemRenewalApplicationDate', type: 'date-range'}
    ]
 };
+
 exports.grid = {
-    columns: ['systemRenewalApplicationNo', 'proposer','systemRenewalApplicationDate'],
-    filterToolbar: true,
-    fixedHeader: true,
-    numberColumn: true,
-    multiple: true,
-    defaultOrder: 'createdTime-desc'
+  columns: ['systemRenewalApplicationNo', 'proposer','systemRenewalApplicationDate', 'applicationDepartment'
+  ],
+  filterToolbar: true,
+  fixedHeader: true,
+  numberColumn: true,
+  multiple: true,
+  defaultOrder: 'createdTime-desc'
 };
+
 exports.exporting = {
   template: 'informationwork/systemRenewalApplication/systemRenewalApplicationModule.xls',
   fileName: '系统主机更新申请'
 };
+
 exports.operators = {
-    exportExcel: { label: '导出', icon: 'zicon-outexcel', group: '30-refresh', order: 10, show: 'unselected', style: 'btn-pink' }
+  exportExcel: { label: '导出', icon: 'zicon-outexcel', group: '30-refresh', order: 10, show: 'unselected', style: 'btn-pink' }
 };
 exports.doWithRouter = function(router) {
-  //导出
   router.get('/export-excel', mark('services', 'commons/export-excel', 'informationwork/systemRenewalApplication').on(function (exportXlsSvc, systemRenewalApplicationSvc, request) {
-      var options = request.params,
-          result;
+    var options = request.params, result;
 
-      options = exportXlsSvc.dealParameters(options, systemRenewalApplicationSvc, new SystemRenewalApplication());
+    options = exportXlsSvc.dealParameters(options, systemRenewalApplicationSvc, new SystemRenewalApplication());
+    result = systemRenewalApplicationSvc.exportExcel(options, exports.exporting.template, exports.exporting.fileName);
 
-      result = systemRenewalApplicationSvc.exportExcel(options, exports.exporting.template, exports.exporting.fileName);
-
-      return json({flag: result.flag, filename: result.filename});
+    return json({flag: result.flag, filename: result.filename});
   }))
 }
