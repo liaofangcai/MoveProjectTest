a5d96825b66ecce49ad5ffa03b48a5ff6ec232eb
luohaibo <luohaibo@zyeeda.com>
2015-12-23 17:05:56 +0800

    信息作业,资产清册代码规范修改

--- src/main/webapp/WEB-INF/app/experiment/propertycleanbook.feature/service.js
+++ src/main/webapp/WEB-INF/app/experiment/propertycleanbook.feature/service.js
@@ -19,27 +19,22 @@ exports.createService = function() {
                 entity, entities,
                 meta = resolver.resolveEntity(PropertyCleanBook),
                 dateTimeStr = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()),
-                dateSdf = new SimpleDateFormat("yyyy-MM-dd")
-
+                dateSdf = new SimpleDateFormat("yyyy-MM-dd"),
             entities = commExpService.createService().listEntities(options, meta);
-
             // 按照自己的要求处理数据
             for (i = 0; i < entities.size(); i++) {
                 entity = entities.get(i);
-
                 vo = commExpService.createService().convertEntityToObj(entity);
-                if(null !== entity.property_date){
-                     vo.property_date = dateSdf.format(entity.property_date);
+                if(null !== entity.propertyDate){
+                     vo.propertyDate = dateSdf.format(entity.propertyDate);
                  }
-                 if (null!==entity.purchasedate) {
-                    vo.purchasedate=dateSdf.format(entity.purchasedate);
+                 if (null!==entity.purchaseDate) {
+                    vo.purchaseDate=dateSdf.format(entity.purchaseDate);
                  }
                 vos.add(vo);
             }
-
             beans.put('propertycleanbooks', vos);
             beans.put('footer', '操作时间:' + dateTimeStr);
-
             return commExpService.createService().exportExcel(beans, exportModule, exportFileName);
         })
 
