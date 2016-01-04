32a63fef1189707a21595a661571641386292a5b
luohaibo <luohaibo@zyeeda.com>
2016-01-04 16:30:18 +0800

    信息作业--------研发需求申请
    1.添加打印功能

---
+++ src/main/webapp/scripts/app/commons/print-researchdemand.feature/views/tripapply-printarea.js
@@ -0,0 +1,37 @@
+(function() {
+    define(["jquery", "underscore"], function($, _) {
+        return {
+            avoidLoadingHandlers: true,
+            size: 'large',
+            extend: {
+                templateHelpers: function() {
+
+                    var tripApplys = [],
+                       selectedDataIds = this.feature.selectedDataIds,
+                        i, l,
+                        approvalList = [];
+
+                    if (this.feature.printData.length > 0) {
+                        tripApplys = this.feature.printData;
+                    }else{
+                        $.ajax({
+                            url: 'invoke/scaffold/experiment/researchdemand/get-researchdemand-by-id',
+                            type: 'get',
+                            async: false,
+                            data: {
+                                selectedDataIds: selectedDataIds
+                            }
+                        }).done(function(data) {
+                            if(!_.isArray(data.tripApplys)){
+                                tripApplys.push(data.tripApplys);
+                            } else {
+                                tripApplys = data.tripApplys;
+                            }
+                        });
+                    }
+                    return {tripApplys: tripApplys};
+                }
+            }
+        };
+    });
+}).call(this);
